43474f5f80107d3799cce253cada2babf0c41770
siromiji <mnmn82018282@gmail.com>
2025-09-11 17:37:45 +0900

    250911 HEADER01 진행중

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function Header(){
+  return(
+    <>
+      <h1>Green Travel</h1>
+      <div className='wonImg'>
+        <img src="../../assets/won.png" alt="태극원" />
+        </div>
+    
+    </>
+  )
+}
+
+export default Header;
