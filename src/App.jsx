104c2aad36b3cd118694ac47b8215faeca651a53
siromiji <mnmn82018282@gmail.com>
2025-09-11 12:59:17 +0900

    250911 브런치 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>SIRO_MIJI</h1>
+      <h1>SIRO_MIJI_GREEN</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
